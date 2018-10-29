693fe7306a4852269e2219a3167efaf92e665081
Benjaminlagos <44414350+Benjaminlagos@users.noreply.github.com>
2018-10-29 14:49:39 -0600

    Add files via upload

---
+++ header.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+	$(window).scroll(function(){
+		if ( $(this).scrolltop() > 0 ){
+			$('header').addclass('header2');
+		} else{
+			$('header').removeclass('header2');
+		}
+	});
+
+});
